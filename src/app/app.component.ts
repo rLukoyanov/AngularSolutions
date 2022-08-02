4074f934737eeb005d9a5f09f583a9c887685031
rLukoyanov <rulukoyanov@gmail.com>
2022-08-02 17:08:34 +0300

    pass Solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'first-assigmant-solution';
+  clicks = ['cliked'];
+  hide = true;
+  blue = false;
+
+  onToggleDetails() {
+    this.hide = !this.hide;
+    this.clicks.push('cliked');
+
+    if (this.clicks.length >= 5) {
+      this.blue = true;
+    }
+  }
 }
